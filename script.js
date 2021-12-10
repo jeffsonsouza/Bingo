71a141eff10ac1378a52f15db1ab3290e8b17e38
jeffsonsouza <87528973+jeffsonsouza@users.noreply.github.com>
2021-12-10 15:36:24 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,26 @@
+let vetor = [];
+const LIMITE = 75
+const MAX = 76
+
+function sortear(){
+    let lista = document.getElementById("lista").innerHTML;
+    let numero;
+    do{
+        if (isLimite()) {
+            numero = parseInt(Math.random() * (MAX - 1) + 1, 10);
+        }
+    }while(isLimite() && presente(numero))
+
+    if (isLimite()) {
+        vetor.push(numero);
+        lista += "<li>"+ numero + "</li>"
+        document.getElementById("lista").innerHTML = lista;
+
+        document.querySelector('#sorteio').innerHTML = numero;
+    }
+    console.log(vetor);
+    console.log(vetor.length);
+}
+
+const presente = (numero) => vetor.indexOf(numero) != -1;
+const isLimite = () =>  vetor.length < LIMITE
